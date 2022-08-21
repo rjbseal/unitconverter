c207942cbdf9763b9a13a880b634db7308b83f72
Robin <rjblackborow@gmail.com>
2022-08-21 21:44:59 +0100

    added function formula

--- index.js
+++ index.js
@@ -3,3 +3,21 @@
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */
+
+const units = document.getElementById("units-input")
+const btnConvert = document.getElementById("btn-convert")
+const convMeters = document.getElementById("conv-meters")
+
+btnConvert.addEventListener("click", function() {
+    convMeters.textContent = `${units.value} meters = ${convertMetersToFeet(units.value)} feet | ${units.value} feet = ${convertFeetToMeters(units.value)} meters`
+})
+
+function convertMetersToFeet (meters) {
+    let result = meters * 3.281;
+    return result.toFixed(2)
+}
+function convertFeetToMeters (feet) {
+    let result = feet / 3.281
+    return result.toFixed(2)
+}
+
